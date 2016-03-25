789a497df1fb84b4bd7c931ab75095331f79c650
Gene <red0@yandex.ru>
2016-03-24 20:25:56 -0700

    about page

--- routes.py
+++ routes.py
@@ -6,7 +6,11 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
+@app.route("/about")
+def about():
+	return render_template("about.html")
 
+	
 if __name__ == '__main__':
     #app.run()
     app.run(host='0.0.0.0', port=8081, debug=True)
