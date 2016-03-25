6abbe4366d0b04b3182ce48e3b05fa14b6058d45
Gene <red0@yandex.ru>
2016-03-24 21:10:17 -0700

    models.py

--- routes.py
+++ routes.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, url_for
-
+from models import db
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://localhost/packtpub'
+db.init_app(app)
 @app.route("/")
 def index():
 	return render_template('index.html')
@@ -10,7 +11,7 @@ def index():
 def about():
 	return render_template("about.html")
 
-	
+
 if __name__ == '__main__':
     #app.run()
     app.run(host='0.0.0.0', port=8081, debug=True)
